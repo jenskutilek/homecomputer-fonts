29ad15e3953ba0cb56e91a73c27f6a5b7ca4cb38
Jens Kutilek <webmail@kutilek.de>
2023-06-22 09:25:30 +0200

    Remove old patches and scripts

--- scripts/2woff.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-from os.path import basename, join
-from fontTools.ttLib import TTFont
-
-for arg in argv[1:]:
-    f = TTFont(arg)
-    f.flavor = "woff2"
-    base, suffix = basename(arg).rsplit(".", 1)
-    f.save(join("fonts", "webfonts", f"{base}.woff2"))
